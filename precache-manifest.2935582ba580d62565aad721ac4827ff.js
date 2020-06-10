ffe1bea0a6435f3a1b1bd583e811ff8ae8b02ff5
Sidhant <sdsd011235@gmail.com>
2020-06-10 23:16:26 +0530

    Updates

---
+++ precache-manifest.2935582ba580d62565aad721ac4827ff.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a3e0524148501b8870dff16c1b1640b",
+    "url": "/prakhar/index.html"
+  },
+  {
+    "revision": "cf97f383216e98c549ab",
+    "url": "/prakhar/static/css/main.036eee11.chunk.css"
+  },
+  {
+    "revision": "451ec62968583b488929",
+    "url": "/prakhar/static/js/2.d635babb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/prakhar/static/js/2.d635babb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf97f383216e98c549ab",
+    "url": "/prakhar/static/js/main.50beec27.chunk.js"
+  },
+  {
+    "revision": "3916b4ef1776544d0c17",
+    "url": "/prakhar/static/js/runtime-main.f7acc4a4.js"
+  },
+  {
+    "revision": "c1f2b52d3260e918a92d9af9b00d15c7",
+    "url": "/prakhar/static/media/JNBaliya.c1f2b52d.jpeg"
+  },
+  {
+    "revision": "88c1c00e065f17190a2763791d935088",
+    "url": "/prakhar/static/media/Location.88c1c00e.svg"
+  },
+  {
+    "revision": "84b3df51125df12f263f640323b92c67",
+    "url": "/prakhar/static/media/Mail.84b3df51.svg"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/prakhar/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "3cf7e68653e282ef3cb91b675a462f97",
+    "url": "/prakhar/static/media/SanjayDhar.3cf7e686.jpeg"
+  },
+  {
+    "revision": "b3b29dd26d14a92f7fb0e935afd41b03",
+    "url": "/prakhar/static/media/Telephone.b3b29dd2.svg"
+  },
+  {
+    "revision": "06ce309339d2f5a3ea83e27242aed9cb",
+    "url": "/prakhar/static/media/aboutus.06ce3093.jpg"
+  },
+  {
+    "revision": "61ce5f546546f5adc5e89b849c4c42db",
+    "url": "/prakhar/static/media/abt-us.61ce5f54.jpg"
+  },
+  {
+    "revision": "fe8506ca91fe0c25479b1421203a896e",
+    "url": "/prakhar/static/media/abtus.fe8506ca.jpg"
+  },
+  {
+    "revision": "0ae74b4dd41616462335e532b3ff0887",
+    "url": "/prakhar/static/media/activity.0ae74b4d.svg"
+  },
+  {
+    "revision": "6600b9ae839ad1ad512426e3dade7644",
+    "url": "/prakhar/static/media/announces.6600b9ae.jpg"
+  },
+  {
+    "revision": "33b6821157490f4e4b6ad8fca2b35345",
+    "url": "/prakhar/static/media/giftedChild.33b68211.jpg"
+  },
+  {
+    "revision": "14b9d835e4129ba712ce5c2dc015b790",
+    "url": "/prakhar/static/media/identify.14b9d835.svg"
+  },
+  {
+    "revision": "f92087943ea65f1f164cbb37c31c52e6",
+    "url": "/prakhar/static/media/poster.f9208794.jpeg"
+  },
+  {
+    "revision": "d4bfe39355efef8bb8aad2718f328cdb",
+    "url": "/prakhar/static/media/prakharLogo.d4bfe393.jpeg"
+  },
+  {
+    "revision": "520ebd6482397364760d303b247881cd",
+    "url": "/prakhar/static/media/talent.520ebd64.svg"
+  },
+  {
+    "revision": "6f8b4c75009b4548af91e9c002e6285b",
+    "url": "/prakhar/static/media/vision.6f8b4c75.jpg"
+  }
+]);
