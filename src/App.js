858d311f52c6bd67522a91220c13390e369c643d
Sidhant <sdsd011235@gmail.com>
2020-07-18 17:19:48 +0530

    removed lazy loading updated pic

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import React, { lazy, Suspense } from 'react';
+import React, { Suspense } from 'react';
 import './App.css';
 import NavBarComponent from './navbar';
 import Jumbo from './Jumbotron';
 import Container from 'react-bootstrap/Container';
-const Registration = lazy(() => import('./registration')); 
-const About = lazy(() => import('./about'));
-const Footer = lazy(() => import('./footer'));
-const Team = lazy(() => import('./team'));
-const Vision = lazy(() => import('./vision'));
+import Registration from './registration';
+import About from './about';
+import Footer from './footer';
+import Team from './team';
+import Vision from './vision';
 
 function App() {
   return (
-      <Container fluid className = 'p-0 m-0 overflow-hidden'>
-      <NavBarComponent/>
-      <Jumbo/>
-      <Suspense fallback = {<h1>Still Loading....</h1>}>
-         <Vision/>
-         <About/>
-         <Registration/>
-         <Team/>
-         <Footer/>
+    <Container fluid className="p-0 m-0 overflow-hidden">
+      <NavBarComponent />
+      <Jumbo />
+      <Suspense fallback={<h1>Still Loading....</h1>}>
+        <Vision />
+        <About />
+        <Registration />
+        <Team />
+        <Footer />
       </Suspense>
       {/* <img src = {poster} alt = 'poster'className = 'img-fluid w-100'/> */}
-      </Container>
-   );
+    </Container>
+  );
 }
 
 export default App;
--- src/team.js
+++ src/team.js
@@ -1,60 +1,104 @@
-import React , { Component } from 'react';
+import React, { Component } from 'react';
 import './team.css';
 import { Row, Col } from 'react-bootstrap';
 import sanjay from './assets/team/SanjayDhar.jpg';
 import jnbaliya from './assets/team/JNBaliya.jpg';
-import nirmohi  from './assets/team/nirmohiUncle.jpg'
+import nirmohi from './assets/team/nirmohiUncle.jpg';
+import dlitt from './assets/team/dlittphD.jpeg';
 
 class Team extends Component {
-    render() {
-        return (
-            <div className = 'text-center team-wrapper mt-lg-5 mt-3'>
-                <h1>The Leadership Team</h1>
-                <p>Know the people who lead team PRAKHAR to achieve the vision &amp; goals</p>
-                <Row className = 'w-75 mx-auto team-horizontal mt-4'>
-                    <Col className = 'col-md-3 col-12'><img src = {sanjay} alt = 'team member: Sanjay Dhar' className = 'img-fluid'/></Col>
-                    <Col className = 'col-md-9 col-12 text-left team-member mb-4'>
-                        <h5>Sanjay Dhar</h5>
-                        <p className = 'team-member-role'>Founder And Managing Trustee</p>
-                        <p className = 'team-member-description'>Prakhar was founded in Delhi by Mr.Sanjay Dhar, an Engineer by profession with industry experience of more than 30 years at National &amp; International levels</p>
-                    </Col>
-                </Row>
+  render() {
+    return (
+      <div className="text-center team-wrapper mt-lg-5 mt-3">
+        <h1>The Leadership Team</h1>
+        <p>
+          Know the people who lead team PRAKHAR to achieve the vision &amp;
+          goals
+        </p>
+        <Row className="w-75 mx-auto team-horizontal mt-4">
+          <Col className="col-md-3 col-12">
+            <img
+              src={sanjay}
+              alt="team member: Sanjay Dhar"
+              className="img-fluid"
+            />
+          </Col>
+          <Col className="col-md-9 col-12 text-left team-member mb-4">
+            <h5>Sanjay Dhar</h5>
+            <p className="team-member-role">Founder And Managing Trustee</p>
+            <p className="team-member-description">
+              Prakhar was founded in Delhi by Mr.Sanjay Dhar, an Engineer by
+              profession with industry experience of more than 30 years at
+              National &amp; International levels
+            </p>
+          </Col>
+        </Row>
 
-                <Row className = 'w-75 mx-auto mt-4'>
-                    <Col className = 'col-md-4 col-12'>
-                    <Row>
-                    <Col className = 'col-12'><img src = { nirmohi } alt = 'team member: Nirmohi' className = 'img-fluid'/></Col>
-                    <Col className = 'col-12 text-left team-member mb-4'>
-                        <h5>Padma Shree Prof. Shiv Nirmohi</h5>
-                        <p className = 'team-member-role'>Member: Advisory Board </p>
-                        <p className = 'team-member-description'>J.N.Baliya, an educationist &amp; pioneer in the field of education with 33 years of long experience in teaching at universities under the phrase “Kindling Brilliant Minds”.</p>
-                    </Col>
-                </Row>
-                </Col>
-                <Col className = 'col-md-4 col-12'>
-                    <Row>
-                    <Col className = 'col-12'><img src = {jnbaliya} alt = 'team member: JN Baliya' className = 'img-fluid'/></Col>
-                    <Col className = 'col-12 text-left team-member mb-4'>
-                        <h5>Dr. Mrityunjay Sharma D.Lit Ph.D. Music</h5>
-                        <p className = 'team-member-role'>Member: Advisory Board </p>
-                        <p className = 'team-member-description'>J.N.Baliya, an educationist &amp; pioneer in the field of education with 33 years of long experience in teaching at universities under the phrase “Kindling Brilliant Minds”.</p>
-                    </Col>
-                </Row>
-                </Col>
-                <Col className = 'col-md-4 col-12'>
-                    <Row>
-                    <Col className = 'col-12'><img src = {jnbaliya} alt = 'team member: JN Baliya' className = 'img-fluid'/></Col>
-                    <Col className = 'col-12 text-left team-member mb-4'>
-                        <h5>J.N Baliya</h5>
-                        <p className = 'team-member-role'>Member: Advisory Board </p>
-                        <p className = 'team-member-description'>J.N.Baliya, an educationist &amp; pioneer in the field of education with 33 years of long experience in teaching at universities under the phrase “Kindling Brilliant Minds”.</p>
-                    </Col>
-                </Row>
-                </Col>
-                </Row>
-            </div>
-        )
-    }
+        <Row className="w-75 mx-auto mt-4">
+          <Col className="col-md-4 col-12">
+            <Row>
+              <Col className="col-12">
+                <img
+                  src={nirmohi}
+                  alt="team member: Nirmohi"
+                  className="img-fluid"
+                />
+              </Col>
+              <Col className="col-12 text-left team-member mb-4">
+                <h5>Padma Shree Prof. Shiv Nirmohi</h5>
+                <p className="team-member-role">Member: Advisory Board </p>
+                <p className="team-member-description">
+                  J.N.Baliya, an educationist &amp; pioneer in the field of
+                  education with 33 years of long experience in teaching at
+                  universities under the phrase “Kindling Brilliant Minds”.
+                </p>
+              </Col>
+            </Row>
+          </Col>
+          <Col className="col-md-4 col-12">
+            <Row>
+              <Col className="col-12">
+                <img
+                  src={dlitt}
+                  alt="team member: D.Lit PhD"
+                  className="img-fluid"
+                />
+              </Col>
+              <Col className="col-12 text-left team-member mb-4">
+                <h5>Dr. Mrityunjay Sharma D.Lit Ph.D. Music</h5>
+                <p className="team-member-role">Member: Advisory Board </p>
+                <p className="team-member-description">
+                  J.N.Baliya, an educationist &amp; pioneer in the field of
+                  education with 33 years of long experience in teaching at
+                  universities under the phrase “Kindling Brilliant Minds”.
+                </p>
+              </Col>
+            </Row>
+          </Col>
+          <Col className="col-md-4 col-12">
+            <Row>
+              <Col className="col-12">
+                <img
+                  src={jnbaliya}
+                  alt="team member: JN Baliya"
+                  className="img-fluid"
+                />
+              </Col>
+              <Col className="col-12 text-left team-member mb-4">
+                <h5>J.N Baliya</h5>
+                <p className="team-member-role">Member: Advisory Board </p>
+                <p className="team-member-description">
+                  J.N.Baliya, an educationist &amp; pioneer in the field of
+                  education with 33 years of long experience in teaching at
+                  universities under the phrase “Kindling Brilliant Minds”.
+                </p>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
 
 export default Team;
